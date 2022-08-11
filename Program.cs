1ae2da3c8b9329ca3f7b16802d82b21b8c14372e
A-Skryabin <109134009+A-Skryabin@users.noreply.github.com>
2022-08-11 21:09:20 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if(numbera > numberb)
+{
+    Console.WriteLine($"{numbera}");
+}
+
+if(numbera < numberb)
+{
+    Console.WriteLine($"{numberb}");
+}
