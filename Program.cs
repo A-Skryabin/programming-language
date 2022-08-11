1a3a2c27a1b83ac0bc450d52836f3b7d2b83ae6a
A-Skryabin <109134009+A-Skryabin@users.noreply.github.com>
2022-08-11 21:14:07 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 int numbera = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int numberb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberc = Convert.ToInt32(Console.ReadLine());
 if(numbera > numberb)
 {
     Console.WriteLine($"{numbera}");
 }
 
-if(numbera < numberb)
+if(numberb > numberc)
 {
     Console.WriteLine($"{numberb}");
+}
+
+if(numberc > numbera)
+{
+    Console.WriteLine($"{numberc}");
 }
