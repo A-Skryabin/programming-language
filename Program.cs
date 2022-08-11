38b045b926df0ff4292930a5704c2b07ba0db548
A-Skryabin <109134009+A-Skryabin@users.noreply.github.com>
2022-08-11 21:22:29 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+int numberb = 2;
+if(numbera % numberb == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
