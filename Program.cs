1c3f76375cf5331ee7ee3e961df082e75e7702ec
A-Skryabin <109134009+A-Skryabin@users.noreply.github.com>
2022-08-24 17:12:33 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿Console.WriteLine("Введите первое число: ");
-int numbera = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int numberb = Convert.ToInt32(Console.ReadLine());
-if(numbera > numberb)
-{
-    Console.WriteLine($"{numbera}");
-}
+﻿// Задача 34: Задайте массив, заполнен. случайн. положительн. трёхзнач. числами. 
+// Напишите программу, которая покажет коли-во чётн. чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
-if(numbera < numberb)
+Console.WriteLine ("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, из них {count} чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
 {
-    Console.WriteLine($"{numberb}");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
 }
