59339fab1c245ccff942c712eecc07cf5e7c5ddf
A-Skryabin <109134009+A-Skryabin@users.noreply.github.com>
2022-09-04 22:28:50 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,65 @@
-﻿Console.WriteLine("Введите первое число: ");
-int numbera = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int numberb = Convert.ToInt32(Console.ReadLine());
-if(numbera > numberb)
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int InputInt(string output)
 {
-    Console.WriteLine($"{numbera}");
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
 
-if(numbera < numberb)
+void FillArrayRandomNumbers(int[,] array)
 {
-    Console.WriteLine($"{numberb}");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); 
+        }
+    }
 }
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("Введите размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("А");
+PrintArray(matrixA);
+//Console.WriteLine();
+Console.WriteLine("В");
+PrintArray(matrixB);
+//Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В:");
+PrintArray(matrixC);
